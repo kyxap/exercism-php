a24e464bebbb9c2976c6080f7106aa82f1f47bed
Roman Kukharuk <rkukharuk@gmail.com>
2017-07-16 21:47:39 -0700

    done :P

--- hamming/hamming.php
+++ hamming/hamming.php
@@ -7,7 +7,15 @@
 
 function distance($a, $b)
 {
-    //
-    // YOUR CODE GOES HERE
-    //
+
+    if (strlen($a) != strlen($b)) {
+        throw new InvalidArgumentException();
+    }
+    $count = 0;
+    for( $i = 0; $i < strlen($a); $i++ ) {
+      if ($a{$i} != $b{$i}) {
+        $count++;
+      }
+    }
+    return $count;
 }
